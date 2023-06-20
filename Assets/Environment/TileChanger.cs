440e3ee5ecb873e857822fd8ab8a25dae344f2f0
Cade Calcote <cade.m.calcote@gmail.com>
2023-06-20 12:09:30 -0700

    Change tiles in selected rectangle
    Will now change all tiles between two selected points.

--- Assets/Environment/TileChanger.cs
+++ Assets/Environment/TileChanger.cs
@@ -41,20 +41,19 @@ public class TileChanger : MonoBehaviour
 
             if (_firstClick != _emptyCell && _secondClick != _emptyCell)
             {
-                print("Changing!");
+               // print("Changing!");
                 FindSelectionCorners();
+                ResetStoredPositions();
             }
         }
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            ChangeTile();
+            ChangeTile(CaptureMousePosition());
         }
     }
 
-    void ChangeTile()
+    void ChangeTile(Vector3Int position)
     {
-        Vector3Int position = CaptureMousePosition();
-        print(position);
         _tileMap.SetTile(position, _orangeTile);
     }
 
@@ -87,27 +86,50 @@ public class TileChanger : MonoBehaviour
 
     void FindSelectionCorners()
     {
-        Vector3Int startPoint = _firstClick;
-        Vector3Int endPoint = _secondClick;
+        
 
         int leftMostX = (_firstClick.x > _secondClick.x) ? _firstClick.x : _secondClick.x;
         int topMostY = (_firstClick.y > _secondClick.y) ? _firstClick.y : _secondClick.y;
 
-        print("Start point: (" + leftMostX + ", " + topMostY + ")");
+        //print("Start point: (" + leftMostX + ", " + topMostY + ")");
 
         int rightMostX = (_firstClick.x < _secondClick.x) ? _firstClick.x : _secondClick.x;
         int bottomMostY = (_firstClick.y < _secondClick.y) ? _firstClick.y : _secondClick.y;
 
-        print("End point: (" + rightMostX + ", " + bottomMostY + ")");
+        //print("End point: (" + rightMostX + ", " + bottomMostY + ")");
 
+        Vector3Int startPoint = new Vector3Int(leftMostX, topMostY, 0);
+        Vector3Int endPoint = new Vector3Int(rightMostX, bottomMostY, 0);
+
+        int height = Mathf.Abs(topMostY - bottomMostY) + 1;
+        int width = Mathf.Abs(leftMostX - rightMostX) + 1;
+
+        //print("The rectangle is " + height + " tall and " + width + " wide");
+
+        GetTilesWithinSelection(startPoint, endPoint, height, width);
+    }
+
+    void GetTilesWithinSelection(Vector3Int startPoint, Vector3Int endPoint, int height, int width)
+    {
         Tile[] tileArray;
+        Vector3Int tileToChange;
+
+        for (int row = 0; row < height; row++)
+        {
+            for (int column = 0; column < width; column++)
+            {
+                tileToChange = new Vector3Int(startPoint.x - column, startPoint.y - row, 0);
+                print("Tile to Change: " + tileToChange);
+                ChangeTile(tileToChange);
+            }
+        }        
     }
 
     void ResetStoredPositions()
     {
         _firstClick = _emptyCell;
         _secondClick = _emptyCell;
-        print("Reset!");
+        //print("Reset!");
     }
 }
 
