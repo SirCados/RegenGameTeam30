e4d4d318dbdc9f298339337c6067b6ec6c30ed23
Cade Calcote <cade.m.calcote@gmail.com>
2023-06-20 12:40:44 -0700

    Contextual change
    By checking the name of the tile to be changed, tiles are now able to be changed based on context.

--- Assets/Environment/TileChanger.cs
+++ Assets/Environment/TileChanger.cs
@@ -6,11 +6,15 @@ using UnityEngine.Tilemaps;
 public class TileChanger : MonoBehaviour
 {
     [SerializeField] Tilemap _tileMap;
-    [SerializeField] Tile _orangeTile;
+    [SerializeField] TileBase _badlandBase;
+    [SerializeField] TileBase _midlandBase;
+    [SerializeField] TileBase _healthySoilBase;
     [SerializeField] Camera _camera;
 
     Vector3Int _firstClick;
     Vector3Int _secondClick;
+    //since (0,0,0) is in the playspace, I set a max int point to be the "empty space".
+    //this shouldn't come up, but it is in for error handling/edge cases
     Vector3Int _emptyCell = new Vector3Int(999999999, 999999999, 999999999);
 
     private void Start()
@@ -41,37 +45,30 @@ public class TileChanger : MonoBehaviour
 
             if (_firstClick != _emptyCell && _secondClick != _emptyCell)
             {
-               // print("Changing!");
                 FindSelectionCorners();
                 ResetStoredPositions();
             }
         }
-        if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            ChangeTile(CaptureMousePosition());
-        }
     }
 
-    void ChangeTile(Vector3Int position)
+    void ResetStoredPositions()
     {
-        _tileMap.SetTile(position, _orangeTile);
+        _firstClick = _emptyCell;
+        _secondClick = _emptyCell;
     }
 
     Vector3Int CaptureMousePosition()
     {
-        //get the mouse position
         var screenPosition = Input.mousePosition;
-        //test to ensure that it's within the visible area
+
         if (screenPosition.x < 0 || screenPosition.y < 0 || screenPosition.x > Screen.width || screenPosition.y > Screen.height)
         {
             return _emptyCell;
         }
 
-        //get the tilemap grid position
         var worldPosition = _camera.ScreenToWorldPoint(screenPosition);
         var gridPosition = _tileMap.WorldToCell(worldPosition);
 
-        //is there a tile there?
         var tile = _tileMap.GetTile(gridPosition);
         if (tile != null)
         {
@@ -86,50 +83,52 @@ public class TileChanger : MonoBehaviour
 
     void FindSelectionCorners()
     {
-        
-
         int leftMostX = (_firstClick.x > _secondClick.x) ? _firstClick.x : _secondClick.x;
         int topMostY = (_firstClick.y > _secondClick.y) ? _firstClick.y : _secondClick.y;
-
-        //print("Start point: (" + leftMostX + ", " + topMostY + ")");
+        Vector3Int startPoint = new Vector3Int(leftMostX, topMostY, 0);
 
         int rightMostX = (_firstClick.x < _secondClick.x) ? _firstClick.x : _secondClick.x;
-        int bottomMostY = (_firstClick.y < _secondClick.y) ? _firstClick.y : _secondClick.y;
-
-        //print("End point: (" + rightMostX + ", " + bottomMostY + ")");
-
-        Vector3Int startPoint = new Vector3Int(leftMostX, topMostY, 0);
+        int bottomMostY = (_firstClick.y < _secondClick.y) ? _firstClick.y : _secondClick.y;        
         Vector3Int endPoint = new Vector3Int(rightMostX, bottomMostY, 0);
 
         int height = Mathf.Abs(topMostY - bottomMostY) + 1;
         int width = Mathf.Abs(leftMostX - rightMostX) + 1;
-
-        //print("The rectangle is " + height + " tall and " + width + " wide");
 
         GetTilesWithinSelection(startPoint, endPoint, height, width);
     }
 
     void GetTilesWithinSelection(Vector3Int startPoint, Vector3Int endPoint, int height, int width)
     {
-        Tile[] tileArray;
-        Vector3Int tileToChange;
+        Vector3Int positionOfTheChange;
 
         for (int row = 0; row < height; row++)
         {
             for (int column = 0; column < width; column++)
             {
-                tileToChange = new Vector3Int(startPoint.x - column, startPoint.y - row, 0);
-                print("Tile to Change: " + tileToChange);
-                ChangeTile(tileToChange);
+                positionOfTheChange = new Vector3Int(startPoint.x - column, startPoint.y - row, 0);
+                
+                print("Tile to Change: " + positionOfTheChange);
+                DetermineNewTile(positionOfTheChange);
             }
-        }        
+        }
+    }
+    void DetermineNewTile(Vector3Int positionOfTheChange)
+    {
+        TileBase tileAtPosition = _tileMap.GetTile(positionOfTheChange);
+
+        if (tileAtPosition.name == _badlandBase.name)
+        {
+            ChangeTile(positionOfTheChange, _midlandBase);
+        }
+        else if (tileAtPosition.name == _midlandBase.name)
+        {
+            ChangeTile(positionOfTheChange, _healthySoilBase);
+        }
     }
 
-    void ResetStoredPositions()
+    void ChangeTile(Vector3Int positionOfTheChange, TileBase tileToChangeTo)
     {
-        _firstClick = _emptyCell;
-        _secondClick = _emptyCell;
-        //print("Reset!");
+        _tileMap.SetTile(positionOfTheChange, tileToChangeTo);
     }
 }
 
