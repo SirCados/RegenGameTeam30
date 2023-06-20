e9a70d9eb9a872553a221f3790308bd21ebbfd37
Cade Calcote <cade.m.calcote@gmail.com>
2023-06-19 18:00:57 -0700

    two points, no change
    Able to calculate the rectangle, but still unable to change the squares in the rectangle. Future commit will not use .SetTileBlock

--- Assets/Systems/Grid/TileChanger.cs
+++ Assets/Systems/Grid/TileChanger.cs
@@ -9,27 +9,64 @@ public class TileChanger : MonoBehaviour
     [SerializeField] Tile _orangeTile;
     [SerializeField] Camera _camera;
 
+    Vector3Int _firstClick;
+    Vector3Int _secondClick;
+    Vector3Int _emptyCell = new Vector3Int(999999999, 999999999, 999999999);
+
+    private void Start()
+    {
+        ResetStoredPositions();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        PlayerClick();
+    }
+
+    void PlayerClick()
+    {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            CaptureMousePosition();
+            if (_firstClick == _emptyCell)
+            {
+                _firstClick = CaptureMousePosition();
+                print(_firstClick);
+            }
+            else if (_firstClick != _emptyCell && _secondClick == _emptyCell)
+            {
+                _secondClick = CaptureMousePosition();
+                print(_secondClick);
+            }
+
+            if (_firstClick != _emptyCell && _secondClick != _emptyCell)
+            {
+                print("Changing!");
+                ChangeTilesInSelection();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            ChangeTile();
         }
     }
 
-    void ChangeTile(Vector3Int position)
+    void ChangeTile()
     {
+        Vector3Int position = CaptureMousePosition();
+        print(position);
         _tileMap.SetTile(position, _orangeTile);
     }
 
-    Vector3 CaptureMousePosition()
+    Vector3Int CaptureMousePosition()
     {
         //get the mouse position
         var screenPosition = Input.mousePosition;
         //test to ensure that it's within the visible area
         if (screenPosition.x < 0 || screenPosition.y < 0 || screenPosition.x > Screen.width || screenPosition.y > Screen.height)
-            return new Vector3();
+        {
+            return _emptyCell;
+        }
 
         //get the tilemap grid position
         var worldPosition = _camera.ScreenToWorldPoint(screenPosition);
@@ -39,14 +76,50 @@ public class TileChanger : MonoBehaviour
         var tile = _tileMap.GetTile(gridPosition);
         if (tile != null)
         {
-            ChangeTile(gridPosition);
+            return gridPosition;
         }
         else
         {
             Debug.Log($"No tile at {gridPosition} ");
+            return _emptyCell;
         }
+    }
 
-        return new Vector3();
+    void ChangeTilesInSelection()
+    {
+        Vector3Int firstPoint = Vector3Int.FloorToInt(_firstClick);
+        Vector3Int secondPoint = Vector3Int.FloorToInt(_secondClick);
+        Vector3Int selectionSize;
+
+        if(firstPoint.x != secondPoint.x && firstPoint.y != secondPoint.y)
+        {
+            int height = (int)Mathf.Abs(firstPoint.y - secondPoint.y) + 1;
+            int width = (int)Mathf.Abs(firstPoint.x - secondPoint.x) + 1;
+
+            selectionSize = new Vector3Int(width, height);
+
+            BoundsInt selectionArea = new BoundsInt(firstPoint, selectionSize);
+
+            Tile[] tileArray = new Tile[height*width];
+
+            print("height: " + height + " width: " + width);
+            for (int index = 0; index < tileArray.Length; index++)
+            {
+                tileArray[index] = _orangeTile;
+
+                print(tileArray[index].transform.GetPosition());
+            }
+
+            _tileMap.SetTilesBlock(selectionArea, tileArray);
+            ResetStoredPositions();
+        }
+    }
+
+    void ResetStoredPositions()
+    {
+        _firstClick = _emptyCell;
+        _secondClick = _emptyCell;
+        print("Reset!");
     }
 }
 
