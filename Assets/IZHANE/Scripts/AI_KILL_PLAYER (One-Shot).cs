baf8e605de79831b6df02017ca5639b0017eb5be
Izhane96 <izhane96@gmail.com>
2023-06-21 17:12:50 -0700

    Player & AI Interaction
    6/20/23
    - Player Movement
    - AI Chase character
    - AI Chase Distance < 4 (distanceBetween)
    - AI Kill player one shot (Does not Re-load Level?) "Box Collision 2D"
    - Created 2 type of Health System Code

---
+++ Assets/IZHANE/Scripts/AI_KILL_PLAYER (One-Shot).cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AIKILLPLAYER : MonoBehaviour
+{
+
+    public int Respawn;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(Respawn);
+        }
+    }
+
+}
---
+++ Assets/IZHANE/Scripts/AI_MonsterI (Main_Movement).cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster_AI : MonoBehaviour
+{
+    //Assign game obeject 
+    public GameObject player;
+
+    // Assign the speed of the Ai's movement
+    public float speed;
+
+    private float distance;
+    // Start is called before the first frame update
+
+    public float distanceBetween;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+
+        Vector2 direction = player.transform.position - transform.position;
+
+        direction.Normalize();
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        
+
+        if (distance < distanceBetween) 
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+    }
+    
+}
