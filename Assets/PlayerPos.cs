f2acc1b33174c69474f3cf0ba98c9f9168c8fc8f
clos619 <clos619@users.noreply.github.com>
2023-06-22 19:33:23 -0700

    fixed error showing player position

--- Assets/PlayerPos.cs
+++ Assets/PlayerPos.cs
@@ -2,34 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPos : MonoBehaviour
+public class playerPos : MonoBehaviour
 {
+
     public GameObject player;
-    public Vector2 playerPos;
+    public Vector2 playerPosition;
     public Transform target;
     public Vector3 dirNearPlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-
-        
     }
 
     void FixedUpdate() {
-        GetPlayerPosition();
-    }
-
-    public void GetPlayerPosition() {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         dirNearPlayer = new Vector3(1,0,0);
-        playerPos = target.position + dirNearPlayer;
-        Debug.Log(playerPos);
+        playerPosition = target.position + dirNearPlayer;
+        Debug.Log(playerPosition);
     }
 }
