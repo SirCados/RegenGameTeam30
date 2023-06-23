854b7d96a3bbb048c7ac626ad9d6eb6174ad59fb
Cade Calcote <cade.m.calcote@gmail.com>
2023-06-23 00:28:58 -0700

    Farming setup

--- Assets/Environment/TileChanger.cs
+++ Assets/Environment/TileChanger.cs
@@ -5,11 +5,22 @@ using UnityEngine.Tilemaps;
 
 public class TileChanger : MonoBehaviour
 {
+    public enum TypeOfSeed
+    {
+        NONE,
+        COVER,
+        FEED,
+        HEDGEROW,
+        MONSTER
+    }
+
     [SerializeField] Tilemap _worldMap;
     [SerializeField] Tilemap _selectorMap;
     [SerializeField] Tilemap _plantMap;
     [SerializeField] Tilemap _corruptionMap;
     [SerializeField] TileBase _badlandBase;
+    [SerializeField] TileBase _badlandInfested1;
+    [SerializeField] TileBase _badlandInfested2;
     [SerializeField] TileBase _midlandBase;
     [SerializeField] TileBase _healthySoilBase;
     [SerializeField] TileBase _healthySoilRooty;
@@ -17,7 +28,13 @@ public class TileChanger : MonoBehaviour
     [SerializeField] TileBase _selectorTile;
     [SerializeField] Camera _camera;
 
+    [SerializeField] TileBase _coverCrop;
+    [SerializeField] TileBase _feedCrop;
+    [SerializeField] TileBase _hedgerow;
+    [SerializeField] TileBase _monsterPlant;
+
     bool _isSelecting = false;
+    TypeOfSeed seedToPlant;
     Vector3Int _firstClick;
     Vector3Int _secondClick;
     Vector3Int _currentMousePosition;
@@ -27,6 +44,7 @@ public class TileChanger : MonoBehaviour
     //since (0,0,0) is in the playspace, I set a max int point to be the "empty space".
     //this shouldn't come up, but it is in for error handling/edge cases
     Vector3Int _emptyCell = new Vector3Int(999999999, 999999999, 999999999);
+        
 
     private void Awake()
     {
@@ -36,20 +54,72 @@ public class TileChanger : MonoBehaviour
 
     private void Start()
     {
-        ResetSelector();        
+        ResetSelector();
+        GameObject player = GameObject.Find("player");
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerClick();
+
+        GameObject player = GameObject.Find("player");
+
+        //TempSeedSelector();
+    }
+
+    void TempSeedSelector()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            print("C");
+            seedToPlant = TypeOfSeed.COVER;
+            GetMousePosition();
+        }
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
+            print("H");
+            seedToPlant = TypeOfSeed.HEDGEROW;
+            GetMousePosition();
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            print("F");
+            seedToPlant = TypeOfSeed.FEED;
+            GetMousePosition();
+        }
+
+        if (_isSelecting)
+        {
+            FindSelectionCorners(_firstClick, GetCurrentMousePosition()); ;
+        }
+    }
+
+    void GetMousePosition()
+    {
+        if (_firstClick == _emptyCell)
+        {
+            _firstClick = CaptureMousePosition();
+            SetFirstSelectedTile();
+            _isSelecting = true;
+        }
+        else if (_firstClick != _emptyCell && _secondClick == _emptyCell)
+        {
+            _secondClick = CaptureMousePosition();
+            _isSelecting = false;
+        }
+
+        if (_firstClick != _emptyCell && _secondClick != _emptyCell)
+        {
+            FindSelectionCorners(_firstClick, _secondClick);
+            ResetSelector();
+        }
     }
 
     void PlayerClick()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            //What is being planted? I need to know here before anything else
             if (_firstClick == _emptyCell)
             {
                 _firstClick = CaptureMousePosition();
@@ -81,6 +151,7 @@ public class TileChanger : MonoBehaviour
         _secondClick = _emptyCell;
         _selectorMap.ClearAllTiles();
         _isSelecting = false;
+        seedToPlant = TypeOfSeed.NONE;
     }
 
     Vector3Int GetCurrentMousePosition()
@@ -112,7 +183,7 @@ public class TileChanger : MonoBehaviour
         var tile = _worldMap.GetTile(gridPosition);
         if (tile != null)
         {
-            print(gridPosition);
+            //print(gridPosition);
             return gridPosition;
         }
         else
@@ -153,6 +224,64 @@ public class TileChanger : MonoBehaviour
             {
                 positionOfTheChange = new Vector3Int(startPoint.x - column, startPoint.y - row, 0);
                 DetermineNewTile(positionOfTheChange);
+                //PlantSeed(positionOfTheChange, seedToPlant);
+            }
+        }
+    }    
+
+    void PlantSeed(Vector3Int positionOfTheChange, TypeOfSeed seed)
+    {
+        Tilemap targetMap = _selectorMap;
+        bool isTileGettingPlant = false;
+
+        if (!_isSelecting)
+        {
+            targetMap = _worldMap;
+        }
+
+        TileBase tileAtPosition = targetMap.GetTile(positionOfTheChange);
+        TileBase seedToPlant = _selectorTile;
+
+        if (!_isSelecting && tileAtPosition == _firstSelectedTile)
+        {
+            switch (seed)
+            {
+                case TypeOfSeed.COVER:
+                    if (tileAtPosition == _badlandBase || tileAtPosition == _badlandInfested1 || tileAtPosition == _badlandInfested2 || tileAtPosition == _midlandBase || tileAtPosition == _healthySoilBase)
+                    {
+                        seedToPlant = _coverCrop;
+                    }
+                    break;
+                case TypeOfSeed.HEDGEROW:
+                    if (tileAtPosition == _midlandBase || tileAtPosition == _healthySoilBase)
+                    {
+                        seedToPlant = _hedgerow;
+                    }
+                    break;
+                case TypeOfSeed.FEED:
+                    if (tileAtPosition == _healthySoilBase)
+                    {
+                        seedToPlant = _feedCrop;
+                    }
+                    break;
+                case TypeOfSeed.MONSTER:
+                    if (tileAtPosition == _badlandBase || tileAtPosition == _badlandInfested1 || tileAtPosition == _badlandInfested2)
+                    {
+                        seedToPlant = _feedCrop;
+                    }
+                    break;
+            }
+        }
+
+        if (_isSelecting || (!_isSelecting && tileAtPosition == _firstSelectedTile))
+        {
+            if (isTileGettingPlant)
+            {
+                ChangeTile(_plantMap, positionOfTheChange, _cropTile);
+            }            
+            else
+            {
+                ResetSelector();
             }
         }
     }
