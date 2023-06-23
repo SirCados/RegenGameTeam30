080e4f6b8b044b19bf68ec0439ff31b2902f48b4
clos619 <clos619@users.noreply.github.com>
2023-06-22 19:13:00 -0700

    added another player attack
    right click uses the players melee attack when entering an enemies collision field

---
+++ Assets/Characters/Player/PlayerPos.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPos : MonoBehaviour
+{
+    public GameObject player;
+    public Vector2 playerPos;
+    public Transform target;
+    public Vector3 dirNearPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        
+    }
+
+    void FixedUpdate() {
+        GetPlayerPosition();
+    }
+
+    void GetPlayerPosition() {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        dirNearPlayer = new Vector3(1,0,0);
+        playerPos = target.position + dirNearPlayer;
+        Debug.Log(playerPos);
+    }
+}
--- Assets/Characters/Player/playerAttack.cs
+++ Assets/Characters/Player/playerAttack.cs
@@ -7,7 +7,8 @@ public class playerAttack : MonoBehaviour
 
     public int ID;
     public float speed;
-    public float dmg;
+    public float shotDamage;
+    public float meleeDamage;
 
     public Rigidbody2D fireball;
     public float fireballSpeed = 8f;
@@ -35,7 +36,7 @@ public Transform target;
     public void ProjectileAttack() {
 
         bool lmb = Input.GetMouseButton(0);
-        bool fButton = Input.GetKeyDown(KeyCode.F);
+        
         RaycastHit hit;
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -44,13 +45,24 @@ public Transform target;
             Debug.Log("Scored a hit");
         }
 
+        
+
         if (lmb) {
             Debug.Log("SHOOTING with left mouse click");
             float step = speed * Time.deltaTime;
-         var fireballInst = Instantiate(fireball, transform.position, Quaternion.Euler(new Vector2(0, 0)));
-    Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    fireballInst.velocity = worldPosition;
+            var fireballInst = Instantiate(fireball, transform.position, Quaternion.Euler(new Vector2(0, 0)));
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            fireballInst.velocity = worldPosition;
+            //EnemyHealth.takeDamage(shotDamage);
+        }
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision) {
+        bool rightClick = Input.GetMouseButton(1);
+
+        if (rightClick && collision.gameObject.tag == "Enemy") {
+            Debug.Log("HITTING WITH F");
+            //EnemyHealth.takeDamage(meleeDamage);
         }
     }
     
